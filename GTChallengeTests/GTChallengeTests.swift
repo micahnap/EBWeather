4132cea31cc04b52fa3d251f36f0203a64bfbb20
Micah Napier <mnapier@scentregroup.com>
2020-06-15 23:21:10 +1000

    Initial Commit

---
+++ GTChallengeTests/GTChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  GTChallengeTests.swift
+//  GTChallengeTests
+//
+//  Created by Micah Napier on 15/6/20.
+//  Copyright © 2020 Micah Napier. All rights reserved.
+//
+
+import XCTest
+@testable import GTChallenge
+
+class GTChallengeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
